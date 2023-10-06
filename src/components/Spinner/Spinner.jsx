c2fc1db83075deaf49e24b560900590c76fe3d73
Israel Hamdan <IsraelSantos_20k@outlook.com>
2023-10-06 11:32:41 -0300

    bug do quizState is not defined

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,11 +1,10 @@
+import React from "react";
 import "./Spinner.css";
 
 const Spinner = () => {
   return (
     <div className="spinner-container">
-      <div className="spinner">
-        <div className="inner-spinner"></div>
-      </div>
+      <div className="spinner"></div>
       <div className="loading-text">Carregando...</div>
     </div>
   );
